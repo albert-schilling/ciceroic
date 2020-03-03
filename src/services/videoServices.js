909782c1eaf22f2059f6dd1030a6d8d6aae3ff8f
Albert Schilling <albertschilling@gmx.net>
2020-03-03 17:32:23 +0100

    Add all input of range type

--- src/services/videoServices.js
+++ src/services/videoServices.js
@@ -6,3 +6,16 @@ export function getVideos(id = '') {
     .catch(error => console.error(error))
   return request
 }
+
+export function patchVideo(id = '', data) {
+  const request = fetch(baseUrl + id, {
+    method: 'PATCH',
+    body: JSON.stringify(data),
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .catch(error => console.error(error))
+  return request
+}
