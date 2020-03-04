3b9412147a7f93dcb011c50c81ee6eb34bf2d91a
Albert Schilling <albertschilling@gmx.net>
2020-03-04 14:20:11 +0100

    Move evaluation form into component

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,17 +4,14 @@ import styled from 'styled-components/macro'
 import { evaluationDimensions } from '../data/evaluationDimensions'
 import { getVideos, patchVideo } from '../services/videoServices'
 import UserMessage from './UserMessage'
-import VideoEvaluationInputRange from './VideoEvaluationInputRange'
+import VideoEvaluationComp from './VideoEvaluation'
 
 export default function Video({ videoBasePath, video, setVideo }) {
   let { id } = useParams()
   const returnPath = `/video/${id}`
 
   const initialDimensionsValues = {}
-  evaluationDimensions.map(dimension => {
-    const name = dimension.name
-    return Object.assign(initialDimensionsValues, { [name]: 3 })
-  })
+  setInitialDimenstionsValues()
   const [dimensionsValues, setDimensionsValues] = useState(
     initialDimensionsValues
   )
@@ -29,6 +26,7 @@ export default function Video({ videoBasePath, video, setVideo }) {
         setVideo(res)
       })
   }, [video, setVideo, id])
+
   return (
     <Main>
       <NavLinkStyled exact to="/">
@@ -49,29 +47,15 @@ export default function Video({ videoBasePath, video, setVideo }) {
           <small>{video.date}</small>
         </VideoDetails>
       </VideoInformation>
-      <VideoEvaluation onSubmit={handleSubmit}>
-        <label htmlFor="firstName">
-          First Name
-          <input type="text" name="firstName" id="firstName" />
-        </label>
-
-        <label htmlFor="lastName">
-          Last Name
-          <input type="text" name="lastName" id="lastName" />
-        </label>
-        {evaluationDimensions.map(dimension => {
-          return (
-            <VideoEvaluationInputRange
-              key={dimension.name}
-              name={dimension.name}
-              description={dimension.description}
-              value={dimensionsValues}
-              setValue={setDimensionsValues}
-            />
-          )
-        })}
-        <VideoEvaluationSubmit type="submit">Submit</VideoEvaluationSubmit>
-      </VideoEvaluation>
+      <VideoEvaluationComp
+        video={Video}
+        setVideo={setVideo}
+        id={id}
+        dimensionsValues={dimensionsValues}
+        setDimensionsValues={setDimensionsValues}
+        initialDimensionsValues={initialDimensionsValues}
+        handleSubmit={handleSubmit}
+      />
       <UserMessage
         message={message}
         visibility={messageVisibility}
@@ -148,6 +132,12 @@ export default function Video({ videoBasePath, video, setVideo }) {
     setVideo(video)
     patchVideo(id, video)
   }
+  function setInitialDimenstionsValues() {
+    evaluationDimensions.map(dimension => {
+      const name = dimension.name
+      return Object.assign(initialDimensionsValues, { [name]: 3 })
+    })
+  }
 }
 
 const Main = styled.main`
@@ -201,38 +191,38 @@ const VideoDetails = styled.p`
   margin-bottom: 0;
 `
 
-const VideoEvaluation = styled.form`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  grid-gap: 20px;
-  margin: 20px 0;
-  label {
-    display: grid;
-    grid-template: auto auto auto / 1fr;
-    /* width: calc(50% - 8px); */
-    width: 100%;
-    grid-gap: 8px;
-  }
+// const VideoEvaluation = styled.form`
+//   display: flex;
+//   flex-wrap: wrap;
+//   justify-content: center;
+//   grid-gap: 20px;
+//   margin: 20px 0;
+//   label {
+//     display: grid;
+//     grid-template: auto auto auto / 1fr;
+//     /* width: calc(50% - 8px); */
+//     width: 100%;
+//     grid-gap: 8px;
+//   }
 
-  input[type='text'] {
-    font-size: 1rem;
-    width: 100%;
-  }
-  input[type='range'] {
-    width: 100%;
-  }
-`
+//   input[type='text'] {
+//     font-size: 1rem;
+//     width: 100%;
+//   }
+//   input[type='range'] {
+//     width: 100%;
+//   }
+// `
 
-const VideoEvaluationSubmit = styled.button`
-  margin: 16px 0 4px 0;
-  align-self: center;
-  width: max-content;
-  border: none;
-  padding: 8px;
-  background: var(--primary-bg-color);
-  text-align: center;
-  font-size: 1rem;
-  color: var(--inverse-primary-font-color);
-  text-decoration: none;
-`
+// const VideoEvaluationSubmit = styled.button`
+//   margin: 16px 0 4px 0;
+//   align-self: center;
+//   width: max-content;
+//   border: none;
+//   padding: 8px;
+//   background: var(--primary-bg-color);
+//   text-align: center;
+//   font-size: 1rem;
+//   color: var(--inverse-primary-font-color);
+//   text-decoration: none;
+// `
---
+++ src/components/VideoEvaluation.js
@@ -0,0 +1,145 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+import { evaluationDimensions } from '../data/evaluationDimensions'
+import VideoEvaluationInputRange from './VideoEvaluationInputRange'
+
+export default function VideoEvaluationComp({
+  dimensionsValues,
+  setDimensionsValues,
+  handleSubmit,
+}) {
+  return (
+    <VideoEvaluation onSubmit={event => handleSubmit(event)}>
+      <label htmlFor="firstName">
+        First Name
+        <input type="text" name="firstName" id="firstName" />
+      </label>
+
+      <label htmlFor="lastName">
+        Last Name
+        <input type="text" name="lastName" id="lastName" />
+      </label>
+      {evaluationDimensions.map(dimension => {
+        return (
+          <VideoEvaluationInputRange
+            key={dimension.name}
+            name={dimension.name}
+            description={dimension.description}
+            value={dimensionsValues}
+            setValue={setDimensionsValues}
+          />
+        )
+      })}
+      <VideoEvaluationSubmit type="submit">Submit</VideoEvaluationSubmit>
+    </VideoEvaluation>
+  )
+  // function handleSubmit(event) {
+  //   event.preventDefault()
+
+  //   const form = event.target
+  //   const fullName = `${form.firstName.value} ${form.lastName.value}`
+
+  //   if (form.firstName.value.length === 0) {
+  //     const setFocus = () => {
+  //       form.firstName.focus()
+  //     }
+  //     setMessageCallback(setFocus)
+  //     setMessage(`Please, fill out your first name.`)
+  //     setMessageVisibility('flex')
+  //     return
+  //   }
+  //   if (form.lastName.value.length === 0) {
+  //     const setFocus = () => {
+  //       form.lastName.focus()
+  //     }
+  //     setMessageCallback(setFocus)
+  //     setMessage(`Please, fill out your last name.`)
+  //     setMessageVisibility('flex')
+  //     return
+  //   }
+  //   if (
+  //     video.hasOwnProperty('evaluations') &&
+  //     video.evaluations.find(
+  //       evaluation =>
+  //         evaluation.evaluator.toLowerCase() === fullName.toLowerCase()
+  //     )
+  //   ) {
+  //     setMessage(
+  //       `Thank you for your ambition, ${fullName}, but you have already evaluated this speech.`
+  //     )
+  //     setMessageVisibility('flex')
+  //     return
+  //   }
+
+  //   setEvaluation(event)
+
+  //   form.reset()
+  //   setDimensionsValues(initialDimensionsValues)
+  //   setMessageVisibility('flex')
+  //   setMessage(`Thank you ${fullName}. Your evaluation has been submitted.`)
+  // }
+
+  // function setEvaluation(event) {
+  //   video.hasOwnProperty('evaluations')
+  //     ? console.log(video.evaluations)
+  //     : Object.assign(video, { evaluations: [] })
+  //   const evaluator = `${event.target.firstName.value} ${event.target.lastName.value}`
+  //   const newEvaluation = {
+  //     evaluator,
+  //     date: new Date().getTime(),
+  //     dimensions: [],
+  //   }
+  //   Object.entries(dimensionsValues).map(dimension => {
+  //     newEvaluation.dimensions.push({
+  //       name: dimension[0],
+  //       value: dimension[1],
+  //     })
+  //   })
+
+  //   video.evaluations.push(newEvaluation)
+  //   setVideo(video)
+  //   patchVideo(id, video)
+  // }
+  // function setInitialDimenstionsValues() {
+  //   evaluationDimensions.map(dimension => {
+  //     const name = dimension.name
+  //     return Object.assign(initialDimensionsValues, { [name]: 3 })
+  //   })
+  // }
+}
+
+const VideoEvaluation = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  grid-gap: 20px;
+  margin: 20px 0;
+  label {
+    display: grid;
+    grid-template: auto auto auto / 1fr;
+    /* width: calc(50% - 8px); */
+    width: 100%;
+    grid-gap: 8px;
+  }
+
+  input[type='text'] {
+    font-size: 1rem;
+    width: 100%;
+  }
+  input[type='range'] {
+    width: 100%;
+  }
+`
+
+const VideoEvaluationSubmit = styled.button`
+  margin: 16px 0 4px 0;
+  align-self: center;
+  width: max-content;
+  border: none;
+  padding: 8px;
+  background: var(--primary-bg-color);
+  text-align: center;
+  font-size: 1rem;
+  color: var(--inverse-primary-font-color);
+  text-decoration: none;
+`
