fedb11f9750a7afa9bc1710910901f530e3ae711
Albert Schilling <albertschilling@gmx.net>
2020-03-08 20:58:31 +0100

    Add test to delete all evaluations

---
+++ src/components/Speech/SpeechStatistics.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+export default function SpeechStatistics() {
+  return (
+    <>
+      <section>
+        <h4>Statistics</h4>
+      </section>
+    </>
+  )
+}
