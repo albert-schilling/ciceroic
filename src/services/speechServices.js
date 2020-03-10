18d1bafd360405b818d021616ea7bb878d798e93
Albert Schilling <albertschilling@gmx.net>
2020-03-10 12:43:33 +0100

    Init firestore and delete all evaluations

--- src/services/speechServices.js
+++ src/services/speechServices.js
@@ -1,4 +1,5 @@
 const baseUrl = 'http://localhost:3001/speeches/'
+import { cardsRef } from '../firebase'
 
 export function getSpeeches(id = '') {
   const request = fetch(baseUrl + id)
@@ -6,6 +7,21 @@ export function getSpeeches(id = '') {
     .catch(error => console.error(error))
   return request
 }
+
+// export function getCards() {
+//   return fetchCards()
+// }
+// function fetchCards() {
+//   return cardsRef.get()
+//     .then(querySnapshot => {
+//       let cardsData = []
+//       querySnapshot.forEach(doc => {
+//         cardsData.push(doc.data())
+//       })
+
+//       return cardsData
+//     })
+// }
 
 export function patchSpeech(id = '', data) {
   const request = fetch(baseUrl + id, {
